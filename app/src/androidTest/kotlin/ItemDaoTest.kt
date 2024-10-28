4dd4d57abe11c64066531efb84f3e42a20e712a8
Ian Rodolfo Martínez Campos <s21120181@alumnos.itsur.edu.mx>
2024-10-27 20:31:25 -0600

    Final de la practica

--- app/src/androidTest/kotlin/ItemDaoTest.kt
+++ app/src/androidTest/kotlin/ItemDaoTest.kt
@@ -1,29 +1,29 @@
 package com.example.inventory
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.runner.RunWith
-import com.example.inventory.data.InventoryDatabase
-import com.example.inventory.data.ItemDao
 import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
-import org.junit.Before
-import org.junit.After
-import java.io.IOException
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.inventory.data.InventoryDatabase
 import com.example.inventory.data.Item
+import com.example.inventory.data.ItemDao
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
-
+import org.junit.runner.RunWith
+import java.io.IOException
 
 @RunWith(AndroidJUnit4::class)
 class ItemDaoTest {
+
     private lateinit var itemDao: ItemDao
     private lateinit var inventoryDatabase: InventoryDatabase
-
-    private var item1 = Item(1, "Apples", 10.0, 20)
-    private var item2 = Item(2, "Bananas", 15.0, 97)
+    private val item1 = Item(1, "Apples", 10.0, 20)
+    private val item2 = Item(2, "Bananas", 15.0, 97)
 
     @Before
     fun createDb() {
@@ -43,15 +43,6 @@ class ItemDaoTest {
         inventoryDatabase.close()
     }
 
-    private suspend fun addOneItemToDb() {
-        itemDao.insert(item1)
-    }
-
-    private suspend fun addTwoItemsToDb() {
-        itemDao.insert(item1)
-        itemDao.insert(item2)
-    }
-
     @Test
     @Throws(Exception::class)
     fun daoInsert_insertsItemIntoDB() = runBlocking {
@@ -67,5 +58,45 @@ class ItemDaoTest {
         val allItems = itemDao.getAllItems().first()
         assertEquals(allItems[0], item1)
         assertEquals(allItems[1], item2)
+    }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun daoGetItem_returnsItemFromDB() = runBlocking {
+        addOneItemToDb()
+        val item = itemDao.getItem(1)
+        assertEquals(item.first(), item1)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun daoDeleteItems_deletesAllItemsFromDB() = runBlocking {
+        addTwoItemsToDb()
+        itemDao.delete(item1)
+        itemDao.delete(item2)
+        val allItems = itemDao.getAllItems().first()
+        assertTrue(allItems.isEmpty())
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun daoUpdateItems_updatesItemsInDB() = runBlocking {
+        addTwoItemsToDb()
+        itemDao.update(Item(1, "Apples", 15.0, 25))
+        itemDao.update(Item(2, "Bananas", 5.0, 50))
+
+        val allItems = itemDao.getAllItems().first()
+        assertEquals(allItems[0], Item(1, "Apples", 15.0, 25))
+        assertEquals(allItems[1], Item(2, "Bananas", 5.0, 50))
+    }
+
+    private suspend fun addOneItemToDb() {
+        itemDao.insert(item1)
+    }
+
+    private suspend fun addTwoItemsToDb() {
+        itemDao.insert(item1)
+        itemDao.insert(item2)
     }
 }
